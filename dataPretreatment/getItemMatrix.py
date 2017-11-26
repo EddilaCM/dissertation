3dfdb2f0658aa7b0c73597db465fb0a75c0344eb
cm-diligentBee <745023750@qq.com>
2017-11-26 10:49:59 +0800

    update

--- dataPretreatment/getItemMatrix.py
+++ dataPretreatment/getItemMatrix.py
@@ -1,3 +1,28 @@
 # -*- coding:utf-8 -*-
 # __author__ = 'cm'
+import csv
 from sklearn import preprocessing
+
+
+def one_hot_encoding(dataArr):
+    # one-hot编码过程
+    enc = preprocessing.OneHotEncoder()
+    enc.fit(dataArr)
+    data_hoted = enc.transform(dataArr).toarray()
+    return data_hoted
+
+
+if __name__ == '__main__':
+    # open file named job.csv
+    readfilename = '../dataset/real_job.csv'
+    openRfile = open(readfilename,'r')
+    reader = csv.reader(openRfile)
+    dataArr = []
+    i = 0
+    for row in reader:
+        new_row = [item for item in row]
+        dataArr.append(new_row)
+        i += 1
+    nameArr = dataArr[0]
+    print dataArr
+    print one_hot_encoding(dataArr)
